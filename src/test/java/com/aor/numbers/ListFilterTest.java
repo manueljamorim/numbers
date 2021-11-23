4695c80f7d0e8afe1bff93a46e2d3f18cc9f3ed4
manueljamorim <manueljoaoamorim@gmail.com>
2021-11-23 21:40:41 +0000

    until 9 (PIP is not working)

--- src/test/java/com/aor/numbers/ListFilterTest.java
+++ src/test/java/com/aor/numbers/ListFilterTest.java
@@ -3,7 +3,9 @@ package com.aor.numbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,6 +27,29 @@ public class ListFilterTest {
     public void divisible() {
         List<Integer> expected = Arrays.asList(2,4,-2);
         ListFilterer new_filterer = new ListFilterer(new DivisibleByFilter(2));
+        Assertions.assertEquals(expected, new_filterer.filter(list));
+    }
+
+    @Test
+    public void filtererTestTrue() {
+        List<Integer> expected = list;
+        PositiveFilter truefilter = new PositiveFilter();
+        PositiveFilter stub = Mockito.mock(PositiveFilter.class);
+        Mockito.when(stub.accept(Mockito.anyInt())).thenReturn(true);
+
+        ListFilterer new_filterer = new ListFilterer(stub);
+        Assertions.assertEquals(expected, new_filterer.filter(list));
+    }
+
+    @Test
+    public void filtererTestFalse() {
+        List<Integer> expected = new ArrayList<>();
+
+        PositiveFilter truefilter = new PositiveFilter();
+        PositiveFilter stub = Mockito.mock(PositiveFilter.class);
+        Mockito.when(stub.accept(Mockito.anyInt())).thenReturn(false);
+
+        ListFilterer new_filterer = new ListFilterer(stub);
         Assertions.assertEquals(expected, new_filterer.filter(list));
     }
 
